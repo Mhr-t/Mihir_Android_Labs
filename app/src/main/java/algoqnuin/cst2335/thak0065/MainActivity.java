6c6e2ee3e7f3ffed2695df3e303ef3094fbca81c
Mihir Thakkar <mihir01thakkar@gmail.com>
2024-02-23 19:17:31 -0500

    Finished Lab06

--- app/src/main/java/algoqnuin/cst2335/thak0065/MainActivity.java
+++ app/src/main/java/algoqnuin/cst2335/thak0065/MainActivity.java
@@ -1,10 +1,6 @@
 package algoqnuin.cst2335.thak0065;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -13,68 +9,99 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
-    EditText usernameEditText, passwordEditText;
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.w(TAG, "onStart: MainActivity is started");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.w(TAG, "onPause: MainActivity is Paused");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.w(TAG, "onResume: MainActivity is Resume");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.w(TAG, "onCreate: Main Activity is Stopped");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.w(TAG, "onDestroy: Main Activity is destroyed");
-    }
+    // Declare class variables
+    private EditText passwordEditText;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.w(TAG, "In onCreate() - Loading Widgets");
+        // Load views using findViewById
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
 
-        Button btn1 = findViewById(R.id.loginButton);
-        EditText emailText = findViewById(R.id.emailEditText);
+        // Add OnClickListener to the loginButton
+        loginButton.setOnClickListener(view -> {
+            // Read password string from EditText
+            String password = passwordEditText.getText().toString();
 
-        btn1.setOnClickListener(v -> {
-            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
-            nextPage.putExtra("Email Address", emailText.getText().toString());
-            startActivity(nextPage);
+            // Check password complexity
+            boolean isComplex = checkPasswordComplexity(password);
+
+            // Update TextView based on password complexity result
+            if (isComplex) {
+                Toast.makeText(MainActivity.this, "Your password meets the requirements", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "You shall not pass!", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 
+    /**
+     * Check if the password meets complexity requirements.
+     *
+     * @param password The password string to be checked.
+     * @return True if the password meets complexity requirements, false otherwise.
+     */
+    private boolean checkPasswordComplexity(String password) {
+        boolean foundUpperCase = false;
+        boolean foundLowerCase = false;
+        boolean foundNumber = false;
+        boolean foundSpecial = false;
 
-    private void authenticateUser(String username, String password) {
-        // Simulated authentication logic, replace with actual authentication mechanism
-        if ("admin".equals(username) && "password123".equals(password)) {
-            // Authentication successful
-            Toast.makeText(MainActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
-            // Proceed to next activity or perform desired action
-            // For example, you can start the MainActivity using an Intent
+        // Iterate over each character in the password string
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                foundUpperCase = true;
+            } else if (Character.isLowerCase(c)) {
+                foundLowerCase = true;
+            } else if (Character.isDigit(c)) {
+                foundNumber = true;
+            } else if (isSpecialCharacter(c)) {
+                foundSpecial = true;
+            }
+        }
+
+        // Check if any of the requirements are missing and display Toast messages
+        if (!foundUpperCase) {
+            Toast.makeText(this, "Your password does not have an upper case letter", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundLowerCase) {
+            Toast.makeText(this, "Your password does not have a lower case letter", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundNumber) {
+            Toast.makeText(this, "Your password does not have a number", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundSpecial) {
+            Toast.makeText(this, "Your password does not have a special symbol", Toast.LENGTH_SHORT).show();
+            return false;
         } else {
-            // Authentication failed
-            Toast.makeText(MainActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+            return true; // Password meets complexity requirements
+        }
+    }
+
+    /**
+     * Check if the character is a special character.
+     *
+     * @param c The character to be checked.
+     * @return True if the character is a special character, false otherwise.
+     */
+    private boolean isSpecialCharacter(char c) {
+        switch (c) {
+            case '#':
+            case '$':
+            case '%':
+            case '^':
+            case '&':
+            case '*':
+            case '!':
+            case '@':
+            case '?':
+                return true;
+            default:
+                return false;
         }
     }
 }
